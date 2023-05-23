781e8fed2ec3d2df3d37765fe8ac506eae353c71
Andrea Fermo <andreafermo.sd@gmail.com>
2023-05-23 20:53:10 +0200

    bugfix edit

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -8,7 +8,7 @@
 
         <div class="mb-3">
             <label for="title" class="form-label">Titolo:</label>
-            <input type="text" class="form-control @error('title') is-invalid @enderror " id="title" name="title" value="{{old('title')}}">
+            <input type="text" class="form-control @error('title') is-invalid @enderror " id="title" name="title" value="{{old('title', $project->title)}}">
             @error('title')
                 <div class="invalid-feedback">
                     {{$message}}
@@ -18,7 +18,7 @@
 
         <div class="mb-3">
             <label for="description" class="form-label">Descrizione del progetto:</label>
-            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{old('description')}}</textarea>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description">{{old('description', $project->description)}}</textarea>
             @error('description')
                 <div class="invalid-feedback">
                     {{$message}}
